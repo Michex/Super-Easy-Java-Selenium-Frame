3de0dbce46faac171927bcecf104383cb6e5250b
Michex <michal0sasin@gmail.com>
2018-05-07 16:08:49 +0200

    Add files via upload

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void doBeforeTest() {
+
+    }
+
+    @Test
+    public void doTestMagic() {
+
+    }
+
+    @AfterTest
+    public void doAfterTest(){
+
+
+    }
+}
