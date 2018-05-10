e7ba2d701147931ded6ec3c3a0ca38f45f7a3ea0
msasin <msasin>
2018-05-10 15:48:55 +0200

    make config file

--- src/test/java/tests/SimpleTest.java
+++ src/test/java/tests/SimpleTest.java
@@ -1,5 +1,5 @@
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
+package tests;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,18 +1,23 @@
+package tests;
+
+import config.ConfigFileReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 
     protected WebDriver driver;
+    protected ConfigFileReader config;
 
     @BeforeTest
     public void openBrowserAndUrl() {
-        driver = new FirefoxDriver();
 
-        driver.get("http://www.xsolve.pl");
+        config = new ConfigFileReader();
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get(config.getURL());
 
     }
 
